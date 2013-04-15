357a55c53d4e8aa61145e4fb558f619e484fe236
Rushi Ganmukhi <rushimg@gmail.com>
2013-04-15 02:03:31 -0400

    added initilaizations and set the output

--- source/kmeans.c
+++ source/kmeans.c
@@ -18,12 +18,15 @@
  * rough version of K-means algorithm to highlight the algorithm
  **/
 
-void k_means(float *imageIn)
+float* k_means(float *imageIn)
 {
     // the cluster vector
     int numElements = DIM*DIM;
     size_t size = numElements * sizeof(float);
     float *cluster = (float*) malloc(size);
+    
+    //output image
+    float *imageOut = (float*) malloc(size);
     
     // the centroids or means
     int means = CLUSTERS;
@@ -32,9 +35,16 @@ void k_means(float *imageIn)
     float *numPixelsCentroid  = (float*) malloc(means);/*needed for the update average step*/
                 
     int iters = 0;
-    int i,j,temp;
+    int i,j,k,h,m,n,temp;
     float distance;
     int min_temp = BIGNUM;
+    
+    //initialize step to set everything to zero
+    for ( m = 0; m < means-1; m++) {
+        centroids[j] = 0;
+        accumulator[j] =0;
+        numPixelsCentroid[j] =0;
+    }
     
     while (iters<ITERS) {
         //assignment step-> assign each point to cluster of closest centroid
@@ -49,7 +59,7 @@ void k_means(float *imageIn)
         }
         
         //update step-> set centroid of each cluster as mean
-        for ( i=0; i < numElements-1; i++){
+        for ( k=0; k < numElements-1; k++){
                 temp = cluster[i];
                 accumulator[temp] += imageIn[i];
                 /* for cluster
@@ -58,7 +68,7 @@ void k_means(float *imageIn)
                  */
                 numPixelsCentroid[i]+=1;
         }
-        for ( j = 0; j < means-1; j++) {
+        for ( h = 0; h < means-1; h++) {
             centroids[j] = accumulator[j]/numPixelsCentroid[j];
             //reset
             accumulator[j] =0;
@@ -67,5 +77,13 @@ void k_means(float *imageIn)
         
         iters++;/* currently using iters rather than a convergence*/
     }
+    
+    // set output
+    for ( n=0; n < numElements-1; n++){
+        temp = cluster[n];
+        imageOut[n] = centroids[temp];
+    }
+    
+    return imageOut;
 }
 

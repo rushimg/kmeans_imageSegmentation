16eea405685cf7533eaaf53ba5257452554a93ca
Rushi Ganmukhi <rushimg@gmail.com>
2013-04-15 00:53:57 -0400

    basic k-means algorithm

--- source/kmeans.c
+++ source/kmeans.c
@@ -1,13 +1,49 @@
 /*
- * gcc -01 -o kmeans kmeans.c -Wno-unused-function `pkg-config --cflags opencv` -m32
+ * gcc -01 -o kmeans kmeans.c 
  */
- 
- 
-#include "cxcore.h"
-#include "highgui.h"
 
-int main( int argc, char** argv )
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <float.h>
+
+#define ITERS              2000
+#define TOLERANCE           0.5
+#define DIM                 512
+#define CLUSTERS             16
+#define BIGNUM        100000000
+/**
+ * rough version of K-means algorithm to highlight the algorithm
+ * need to impliment a random creation of centroids
+ * also need to add distance calculations based on rgb values
+ **/
+
+void k_means(float *imageIn, float *centroids)
 {
-    // Load in an image
-    //IplImage* iplImage = cvLoadImage("wiki_sg_800_521.jpg",0);\
+    // the output cluster vector
+    int numElements = DIM;
+    size_t size = numElements*numElements* sizeof(float);
+    float *cluster = (float*) malloc(size);
+                
+    int iters = 0;
+    int i,j;
+    float distance;
+    int min_temp = BIGNUM;
+    
+    while (iters<ITERS) {
+        for ( i=0; i < DIM-1; i++){
+            for ( j = 0; j < DIM-1; j++) {
+                distance = imageIn[i]-centroids[j];/*compare image to centroids*/
+                if (distance < min_temp){
+                    min_temp = distance;
+                    cluster[i] = j; /* assign this point to current cluster*/
+                }
+            }
+        }
+        iters++;/* currently using iters rather than a convergence*/
+    }
 }
+
+
